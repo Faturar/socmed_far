b8b76dcc0fa3b0e63313f7944242dd7139da6b4c
Fatur <64860344+Faturar@users.noreply.github.com>
2023-03-17 00:34:56 +0700

    create edit post method

---
+++ client/lib/posts/editPost.jsx
@@ -0,0 +1,22 @@
+export default async function editPost(id, data) {
+    const fd = new FormData();
+
+    fd.append("userId", 1);
+    fd.append("image", data.image);
+    fd.append("content", data.content);
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}`, {
+        method: "PUT", 
+        mode: "cors",
+        headers: {
+          'Accept': 'application/json',
+        },
+        body: fd,
+    });
+  
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+  
+    return res.json()
+}
