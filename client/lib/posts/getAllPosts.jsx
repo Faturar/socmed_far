f5ccdd09a5c5a0a2af8671bea8837e809b6c8b77
Fatur <64860344+Faturar@users.noreply.github.com>
2023-03-17 00:33:40 +0700

    add get all method

---
+++ client/lib/posts/getAllPosts.jsx
@@ -0,0 +1,11 @@
+export default async function getAllPosts() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, { 
+      cache: 'no-store' 
+    });
+  
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+  
+    return res.json()
+}
